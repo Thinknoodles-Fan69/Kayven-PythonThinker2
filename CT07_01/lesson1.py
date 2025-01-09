157332ebb59aa60cb2169813315849175cc38277
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-01-09 11:04:45 +0000

    End of lesson 1
    Co-authored-by: Cheryse Wee <chxrysx@users.noreply.github.com>

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,83 @@
-print("Hello from lesson 1")
+# print("Hello from lesson 1")
 
+# MinAge = 18
+# user = int(input("How old are you? "))
+# if user < MinAge:
+#     print("Access Denied")
+# else:
+#     print("Welcome!")
+    
+
+
+# If item equals plastic throw item in plastic bin
+# If item equals glass throw item in glass bin
+# If item equals paper throw item in paper bin
+
+# item = str(input("What material is the object you are throwing? "))
+# if item == "Glass" or "glass":
+#     print("Throw it in glass bin")
+# elif item == "Plastic" or "plastic":
+#     print("Throw it in plastic bin")
+# elif item == "Paper" or "paper":
+#     print("Throw it in paper bin")
+# else:
+#     print("There is no bin available for this item")
+
+
+
+# Red = int(1 * 3)
+# Blue = int(2 * 5)
+# Green = int(3 * 4)
+# Sum = str(Red + Green + Blue)
+# print("The total amount you have spend at the sushi restaurant that you had lunch has is $" + Sum)
+
+# redCost = 1
+# blueCost = 2
+# greenCost = 3
+# redTotal = redCost * 3
+# blueTotal = blueCost *5
+# greenTotal = greenCost *4
+# Ans = greenTotal + blueTotal + redTotal
+# print("The total amount you have spend at the sushi restaurant that you had lunch has is $" + str(Ans))
+
+# Sample code:
+# px_red = 1
+# px_blue = 2
+# px_green = 3
+
+# num_red = 3
+# num_blue = 5
+# num_green = 4
+
+# total = (px_red * num_red) + (px_blue * num_blue) + (px_green * num_green)
+
+# print(total)
+
+
+
+
+
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+
+score = int(input("What score did you get for the exam? "))
+if score >= 75:
+    print("Your grade is A! Good Job")
+elif score >= 60:
+    print("Your grade is B! Not bad")
+elif score >= 50:
+    print("Your grade is C! You have to improve")
+else:
+    print("You Fail your exam! Go and see the principal and i will call your parents and cane you with the worlds longest and fattest cane in world history. You will get detention for 365 days for 10 years and do 100000 hours of community service and will get a 1000 warning letter send to your parent!!!!!!!! ")
