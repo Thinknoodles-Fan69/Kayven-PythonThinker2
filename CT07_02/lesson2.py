f923d5aee9903489842ed54d1021ecbf06271742
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-01-23 10:18:25 +0000

    Jan 23, 2025, 10:18 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -202,4 +202,4 @@ Question = "What is for breakfast? = "
 answer = "Stale chips with Skibidi Ohio Sigma Rizzler Gyatty Ligma Kissma Toilet Bowl Sauce"
 guess = input(Question)
 while guess != answer:
-    print("Wrong! Are you freaking kidding me? It is so easy bruh. How hard it is? You did not even try your best lol")
+    print("Wrong! Are you freaking kidding me? It is so easy bruh. How hard it is? You did not even try your best lol. I will send you to Jesus!!!!!!!!!!!!!!")
