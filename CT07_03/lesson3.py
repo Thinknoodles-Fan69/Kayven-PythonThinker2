37284721e20ffffddd52b4f7e0ad2721fe5eae29
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-01-23 10:28:17 +0000

    Jan 23, 2025, 10:28 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -1,2 +1,10 @@
-print("Hello from lesson 3")
+# print("Hello from lesson 3")
 
+# Question = "What is for breakfast? = "
+# answer = "Stale chips with Skibidi Ohio Sigma Rizzler Gyatty Ligma Kissma Toilet Bowl Sauce"
+# guess = input(Question)
+# while guess != answer:
+#     print("Wrong! Are you freaking kidding me? It is so easy bruh. How hard it is? You did not even try your best lol. I will send you to Jesus!!!!!!!!!!!!!!")
+#     guess = input(Question)
+
+# print("Finally correct. HAIYAAA why you take so long?????????Stooopid")
