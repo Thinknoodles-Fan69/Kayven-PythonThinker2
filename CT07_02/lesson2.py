7649a9a567be3023d4c71cfc02aabd0762034d1a
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-01-23 10:27:56 +0000

    Jan 23, 2025, 10:27 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -198,14 +198,7 @@
 
 
 
-Question = "What is for breakfast? = "
-answer = "Stale chips with Skibidi Ohio Sigma Rizzler Gyatty Ligma Kissma Toilet Bowl Sauce"
-guess = input(Question)
-while guess != answer:
-    print("Wrong! Are you freaking kidding me? It is so easy bruh. How hard it is? You did not even try your best lol. I will send you to Jesus!!!!!!!!!!!!!!")
-    guess = input(Question)
 
-print("Finally correct. HAIYAAA why you take so long?????????Stooopid")
       
       
       
