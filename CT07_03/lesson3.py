06f509351c685dfc9305ff9388794cb72b3418dc
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-01-23 11:07:55 +0000

    Jan 23, 2025, 11:07 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -45,7 +45,8 @@ while question != 15:
     if ask != answer:
         print("You are dumb. Wrong!!")
         lives -= 1
-
+    if ask == answer:
+        print("Yeay")
     if lives == 0:
         print("You stoopid Lose! You suck bruh.")    
         break
