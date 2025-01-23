5bda4329419eb2f91ca3d94d0dcb364a22c10c08
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-01-23 10:44:57 +0000

    Jan 23, 2025, 10:44 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -32,4 +32,4 @@ while saving < 100:
     question = float(input("How much do you save today after in school, eating recess, eating lunch, buying football card, betting with your friends, eating dinner, going to the movie, paying money to have some rest, wasting the aircon electricity?"))
     saving += question
 
-
+print("")
