a86f780ba46c6a7e602c23b4516ade0407ecc538
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-06 10:13:51 +0000

    Feb 6, 2025, 10:13 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -14,14 +14,14 @@ Solar = [
 # Solar[3] = "Skibidi Ohio Sigma Rizzler Gyatty Ligma Kissma Toilet Bowl"
 # print(Solar)
 
-zoo = [
-    "monkey",
-    "lion",
-    "deer",
-    "tiger",
-    "elephant",
-    "kangaroo"
-]
+# zoo = [
+#     "monkey",
+#     "lion",
+#     "deer",
+#     "tiger",
+#     "elephant",
+#     "kangaroo"
+# ]
 
 
 # counter = 0
