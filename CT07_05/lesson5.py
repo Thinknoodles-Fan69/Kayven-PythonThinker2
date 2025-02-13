3275b1723319308071075f912bd112788a3f8614
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 10:28:19 +0000

    Feb 13, 2025, 10:28 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -61,6 +61,6 @@ heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 number = heightlist.index(max(heightlist))
 print(str(namelist[number]) + " is the tallest in the class with a height of " + max(heightlist) + "cm.")
 number1 = heightlist.index(min(heightlist))
-print(str(namelist(number1)) + " is the shortest in the class with a height of " + min(heightlist) + "cm.")
+print(str(namelist[number1]) + " is the shortest in the class with a height of " + min(heightlist) + "cm.")
 
 namelist[1]
