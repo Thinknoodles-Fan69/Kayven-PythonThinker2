398391b4fc8d65b8a94b965bfe7682b4e4f21bac
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 10:24:00 +0000

    Feb 13, 2025, 10:24 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -58,6 +58,6 @@ namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
             ]
 heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 
-print(max(heightlist))
+number = (max(heightlist))
 
 print(min(heightlist))
