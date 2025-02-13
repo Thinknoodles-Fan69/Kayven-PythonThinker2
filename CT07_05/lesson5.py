985f3d11fbf1b1e332ae57d7928f6caee1e8b02e
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 10:55:04 +0000

    Feb 13, 2025, 10:55 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -80,12 +80,16 @@ powers = [
 ]
 
 name = input("What is you name? =")
+
 print("Welcome, one and all, to this momentous occasion! Today marks the 50th anniversary pokemon competition final—a place where the best Pokemon player win the grand champion and is titled the world best Pokemon Player. We gather here not just to celebrate a Winner, but to embrace the future. Let the competition begin!")
 print("Here are the rules:")
 print("Each player has a random pokemon with a power to it. Whoever's pokemon power is greater, will win the competition. Let the games begin!")
+
 Pokemon1 = random.choice(pokemons)
 print(name + " pokemons is the " + Pokemon1 + ". Will he achieve victory with this pokemon? Let us give him a round of applause for him getting a good pokemon!")
+
 Pokemon2= random.choice(pokemons)
 while Pokemon2 == Pokemon1:
     Pokemon2= random.choice(pokemons)
 print("AI bot pokemons is the " + Pokemon2 + ". Will he achieve victory with this pokemon? Let us give him a round of applause for him getting a good pokemon!")
+
