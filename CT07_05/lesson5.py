c5d854c315929debe01d0d2d4ad93c0e3b8d5e3e
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 10:57:02 +0000

    Feb 13, 2025, 10:57 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -96,8 +96,7 @@ name = input("What is you name? =")
 print("Welcome, one and all, to this momentous occasion! Today marks the 50th anniversary pokemon competition final—a place where the best Pokemon player win the grand champion and is titled the world best Pokemon Player. We gather here not just to celebrate a Winner, but to embrace the future. Let the competition begin!")
 print("Here are the rules:")
 print("Each player has a random pokemon with a power to it. Whoever's pokemon power is greater, will win the competition. Let the games begin!")
+
 print(name + " pokemon is the " + Pokemon1 + ". Will he achieve victory with this pokemon? Let us give him a round of applause for him getting a good pokemon!")
-
-
 print("AI bot pokemon is the " + Pokemon2 + ". Will he achieve victory with this pokemon? Let us give him a round of applause for him getting a good pokemon!")
 
