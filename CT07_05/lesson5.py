dca9e1c09aaf3fc1d205049d571f939589434efb
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 09:45:38 +0000

    Feb 13, 2025, 9:45 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,13 +1,16 @@
 # print("Hello from lesson 5")
 
-food = [    
-    "Chicken Rice",
-    "Ice cream",
-    "Avacado From Mexico",
-   "Chocolate Ice Blended",
-    "Chilli"
-]
-food.pop(2)
-food.append("Mee")
-for i in food:
-    print(i)
+# food = [    
+#     "Chicken Rice",
+#     "Ice cream",
+#     "Avacado From Mexico",
+#    "Chocolate Ice Blended",
+#     "Chilli"
+# ]
+# food.pop(2)
+# food.append("Mee")
+# for i in food:
+#     print(i)
+
+
+
