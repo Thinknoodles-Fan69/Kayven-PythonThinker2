139cd204ee89b6e5ddd2d152aab26bd31cccb1ed
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 10:54:06 +0000

    Feb 13, 2025, 10:54 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -84,7 +84,7 @@ print("Welcome, one and all, to this momentous occasion! Today marks the 50th an
 print("Here are the rules:")
 print("Each player has a random pokemon with a power to it. Whoever's pokemon power is greater, will win the competition. Let the games begin!")
 Pokemon1 = random.choice(pokemons)
-print(name + " pokemons is the " + Pokemon1 + ". Will he achieve victory with this pokemon?")
+print(name + " pokemons is the " + Pokemon1 + ". Will he achieve victory with this pokemon? Let us give him a round of applause for him getting a good pokemon!")
 Pokemon2= random.choice(pokemons)
 while Pokemon2 == Pokemon1:
     Pokemon2= random.choice(pokemons)
