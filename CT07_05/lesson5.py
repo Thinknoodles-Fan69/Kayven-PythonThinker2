a897e8dcac5fef30718ce4fca013fb7cd68c5e9b
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-02-13 10:50:17 +0000

    Feb 13, 2025, 10:50 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -79,7 +79,7 @@ powers = [
     50, 125, 65, 110, 83
 ]
 print("Welcome, one and all, to this momentous occasion! Today marks the 50th anniversary pokemon competition final—a place where the best Pokemon player win the grand champion and is titled the world best Pokemon Player. We gather here not just to celebrate a Winner, but to embrace the future. Let the competition begin!")
-
+print("Here are the rules")
 Pokemon1 = random.choice(pokemons)
 print
 Pokemon2= random.choice(pokemons)
