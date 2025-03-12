4b9544cc8bf097b15a78349fbc3edab581be55b7
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-03-12 08:08:21 +0000

    Mar 12, 2025, 8:08 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,21 +1,32 @@
 # print("Hello from lesson 8")
-list1 = [3, 2, 1]
-list2 = [6, 5, 5]
-list3 = [9, 8, 7]
-newList = []
-lista = []
-listb = []
-daddy = list1 + list2 + list3
+# list1 = [3, 2, 1]
+# list2 = [6, 5, 5]
+# list3 = [9, 8, 7]
+# newList = []
+# lista = []
+# listb = []
+# daddy = list1 + list2 + list3
 
-for letters in daddy:
-    if letters not in newList:
-        newList.append(letters)
+# for letters in daddy:
+#     if letters not in newList:
+#         newList.append(letters)
 
-newList = sorted(newList)
-midPoint = len(newList) // 2
+# newList = sorted(newList)
+# midPoint = len(newList) // 2
 
-lista = newList[:midPoint]
-listb = newList[midPoint:]
+# lista = newList[:midPoint]
+# listb = newList[midPoint:]
 
-print(lista)
-print(listb)
+# print(lista)
+# print(listb)
+
+
+
+
+
+
+
+
+
+
+
