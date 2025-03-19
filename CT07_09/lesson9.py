97c54b5a2e287b4af2950376cc264bb8b27704cc
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-03-19 07:45:29 +0000

    Mar 19, 2025, 7:45 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,4 +1,4 @@
 # print("Hello from lesson 9")
 
-guess = input("What has to be broken before you can use it? =")
-
+guess = [input("What has to be broken before you can use it? =")]
+isCorrect = False
