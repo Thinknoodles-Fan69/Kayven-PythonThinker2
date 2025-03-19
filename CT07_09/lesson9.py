220997badac13c58fcca9decee622c94680f5a9a
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-03-19 07:49:59 +0000

    Mar 19, 2025, 7:49 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -4,4 +4,6 @@ guess = [input("What has to be broken before you can use it? =")]
 isCorrect = False
 guess = guess.lower()
 guess = guess.split(",")
-    for
+for i in guess:
+    if "egg" in guess:
+        isCorrect = True
