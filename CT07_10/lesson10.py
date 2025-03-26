c3b897a73f8ffd1be419032dcb3620cafd0b98bc
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-03-26 08:26:23 +0000

    Mar 26, 2025, 8:26 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,13 +1,13 @@
 # print("Hello from lesson 10")
 
 
-ans = 0
-one = int(input("Give me your first number:"))
-two = int(input("Give me your second number:"))
-def multiply(no1, no2):
-    print(no1 * no2)
+# ans = 0
+# one = int(input("Give me your first number:"))
+# two = int(input("Give me your second number:"))
+# def multiply(no1, no2):
+#     print(no1 * no2)
 
-multiply(one, two)
+# multiply(one, two)
 
 age = int(input("How old are you?"))
 discount = False
