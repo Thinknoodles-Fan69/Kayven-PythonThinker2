8c313dda20b524a2dfd420141f77bad48a04750b
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-03-26 08:14:47 +0000

    Mar 26, 2025, 8:14 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -2,9 +2,9 @@
 
 
 ans = 0
-one = input("Give me your firstnumber")
-two = input("Give me a second number")
+one = input("Give me your first number:")
+two = input("Give me your second number:")
 def multiply(no1, no2):
     print(no1 * no2)
 
-multiply()
+multiply( i)
