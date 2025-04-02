41b14700f815abc5759d23a435d83fa9a44d558c
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-02 07:48:55 +0000

    Apr 2, 2025, 7:48 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -2,14 +2,17 @@
 import random
 
 
-correct = False
+
+
 def diceGuess(guess):
+    correct = False
     winningNo = random.randint(1, 6)
     if guess == winningNo:
         correct = True
     else:
         correct = False
 
+diceGuess()
 if correct == True:
     print("You got the correct number ")
 else:
