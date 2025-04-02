944b6ad2f853d69bde204d16dae5df0b4c3a391a
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-02 07:55:57 +0000

    Apr 2, 2025, 7:55 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -4,12 +4,28 @@ import random
 
 
 
-def diceGuess(guess):
-    winningNo = random.randint(1, 6)
-    if guess == winningNo:
-        print("You got the correct number ")
-    else:
-        print("Wah! you got it wrong!")
+# def diceGuess(guess):
+#     winningNo = random.randint(1, 6)
+#     if guess == winningNo:
+#         print("You got the correct number ")
+#     else:
+#         print("Wah! you got it wrong!")
 
 
-diceGuess(input("Give me a number: "))
+# diceGuess(input("Give me a number: "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
