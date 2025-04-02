f467e8fb09d1050951a2af8e46420028c223d091
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-02 07:50:21 +0000

    Apr 2, 2025, 7:50 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -5,12 +5,11 @@ import random
 
 
 def diceGuess(guess):
-    guess = False
     winningNo = random.randint(1, 6)
     if guess == winningNo:
         print("You got the correct number ")
     else:
-        correct = False
+        print("Wah! you got it wrong!")
 
 
 if diceGuess(input("Give me a number: ")):
