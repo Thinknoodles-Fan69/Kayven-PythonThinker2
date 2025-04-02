ac669eff9c7ce882e5178f22d546d16a319f311f
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-02 08:19:25 +0000

    Apr 2, 2025, 8:19 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -47,7 +47,7 @@ for row in board:
 
     for col in row:
         print(f"| {count} ", end="")
-        count += 1
 
         if count % 3 == 0:
-            
+            print()
+        count += 1
