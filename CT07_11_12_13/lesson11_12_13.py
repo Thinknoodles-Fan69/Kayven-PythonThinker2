815817292d39b38a39dfd1193003f41faf3b939d
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-09 08:02:57 +0000

    Apr 9, 2025, 8:02 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -68,7 +68,7 @@ def get_player_move(argboard):
 
 
                 if argboard[row][col] == " ":
-                    argboard[row][col] = "X"
+                    argboard[row][col] = argboard[row][col] + "X"
                     break
                 else:
                     print(f"{move+1} is already taken. Choose another please")
