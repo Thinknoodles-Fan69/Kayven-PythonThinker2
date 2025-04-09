bd87b025794c069d851208764d4b2f858098325b
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-09 07:46:01 +0000

    Apr 9, 2025, 7:46 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -74,7 +74,7 @@ while True:
             if board[row][col] == " ":
                 board[row][col] = "x"
             else:
-                print(f"(move+1 is already taken. Choose another please")
+                print(f"{move+1} is already taken. Choose another please")
             # pass
         else:
             print("Eh! Siao Dabor u butter pult an legit numble lerh. Isf u don then i suport adove hiter.")
