9cad7a133e6f218c8cfbd6f26af0353c5df68b16
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-09 08:33:59 +0000

    Apr 9, 2025, 8:33 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -102,8 +102,8 @@ def checkwin(argboard):
         [argboard[0][0], argboard[1][0], argboard[2][0]]
         [argboard[0][1], argboard[1][1], argboard[2][1]]
         [argboard[0][2], argboard[1][2], argboard[2][2]]
-        [argboard[0][0], argboard[0][1], argboard[0][2]]
-        [argboard[0][0], argboard[0][1], argboard[0][2]]
+        [argboard[0][0], argboard[1][1], argboard[2][2]]
+        [argboard[0][2], argboard[1][1], argboard[2][0]]
 
     ]
 
