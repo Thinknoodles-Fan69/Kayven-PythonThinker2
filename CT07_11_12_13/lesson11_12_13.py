2b0977322211fb7b4c6663783eae170d53586797
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-16 07:49:23 +0000

    Apr 16, 2025, 7:49 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -257,7 +257,7 @@ def get_aimove(board, currplayer):
     for i in range(3):
         for j in range(3):
             if board[i][j] == " ":
-                board
+                board[i][j] == currplayer 
     
 board = initboard()
 currentplayer = " "
