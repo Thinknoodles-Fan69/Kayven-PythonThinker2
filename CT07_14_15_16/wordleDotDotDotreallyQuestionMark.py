c6431697e32032ad2084817a784b029326118734
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-04-30 08:08:46 +0000

    Apr 30, 2025, 8:08 AM

--- CT07_14_15_16/wordleDotDotDotreallyQuestionMark.py
+++ CT07_14_15_16/wordleDotDotDotreallyQuestionMark.py
@@ -1,5 +1,20 @@
-with open("FiveLetterWords.csv", "r") as fileobj:
+'''
+Have you played Wordle before? Try it out here >> Wordle - The New York Times (nytimes.com)
 
+In this assignment, you are given a text file containing 5757 5-letter words. 
+
+In this assignment, your task is to create a program that works like wordle. Your program will:
+
+1. Choose a random word from the list
+
+2. Allow you to input a 5-letter word
+
+3. Validate if you have you have the letter in the correct position, correct letter but in the wrong position, or the wrong letter.
+
+4. You can use the console output to print out the statements
+'''
+with open("FiveLetterWords.csv", "r") as fileobj:
+        
         contents = fileobj.read()
 
         print(contents)
