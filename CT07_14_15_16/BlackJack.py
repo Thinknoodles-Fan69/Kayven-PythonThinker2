d1f620c73a8a615e9c692f930f83f5f541401cbb
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2025-05-14 14:37:31 +0000

    May 14, 2025, 2:37 PM

--- CT07_14_15_16/BlackJack.py
+++ CT07_14_15_16/BlackJack.py
@@ -119,7 +119,9 @@ while True:
                 print(">>>Banker is thinking<<<..")
                 time.sleep(random.randint(1, 2))
                 banker_hand.append(deck.pop())
-            
+                show_hand(banker_hand, "banker_show")
+                show_hand(player_hand, "player_show")
+                
             if calculate(banker_hand) > 21:
                 
                 print("Dealer busted!")
